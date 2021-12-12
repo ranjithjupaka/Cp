227cacc38ed9bf6b12b400f62ea1fea4f7e25e6d
ranjithcoder <74724775+ranjithcoder@users.noreply.github.com>
2021-12-12 10:50:32 +0530

    first commit

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// -9*10e18 < long long < 9*10e18, 0 < unsigned long long < 1.8 * 10e19
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> vll;
+typedef pair<ll,ll> pll;
+typedef long double ld ;
+typedef unordered_map<ll,ll> umapll;
+typedef unordered_map<string,ll> umapsll;
+
+#define pb push_back
+#define f first
+#define s second
+
+
+void solve() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll t;
+    cin >> t;
+   while(t--) {
+        solve();
+    }
+}
